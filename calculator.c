f4a39a62eb4da7cc795cacfb0fa51ef8aad4ee3e
rajesh <rajesh.y@everest.engineering>
2021-08-11 09:09:11 +0530

    added the substraction feature

--- calculator.c
+++ calculator.c
@@ -1,3 +1,24 @@
 #include <stdio.h>
 int main(){
+  int first, second,substraction;
+  char op;
+  printf("enter the two numbers");
+     scanf("%f %f", &first, &second);
+   printf("select the operation to perform (-):");
+   scanf("%c", &op);
+  /* write the functions for addition and substractions in the next branches*/
+  if (op=='-')
+  {
+    substraction(first,second);
+     
+    }else{
+      printf("choose the appropriate operation");
+    }
 }
+substraction(int a,int b)
+{
+  int c;
+  c = a-b;
+  printf("the substraction of these two numbers is %d + %d = %d", a,b,c);
+}
+
