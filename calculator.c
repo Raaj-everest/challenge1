b73386ac07edf1d6c149edf040f3834a96714561
rajesh <rajesh.y@everest.engineering>
2021-08-11 08:58:53 +0530

    added the addittion feature

--- calculator.c
+++ calculator.c
@@ -1,3 +1,22 @@
 #include <stdio.h>
 int main(){
+  int first, second;
+  char op;
+  printf("enter the two numbers");
+     scanf("%d %d", &first, &second);
+   printf("select the operation to perform (+):");
+   scanf("%c", &op);
+  /* write the functions for addition and substractions in the next branches*/
+  if(op=='+'){
+      addittion(first ,second);
+       
+    }else{
+      printf("choose the appropriate operation");
+    }
+}
+ addittion(int a,int b)
+{
+      int c;
+      c = a+b;
+      printf("the addition of these two numbers is %d + %d = %d", a,b,c);
 }
